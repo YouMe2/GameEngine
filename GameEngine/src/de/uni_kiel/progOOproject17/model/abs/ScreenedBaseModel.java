db4529d669a0bd1bf0e4610d93ce1527e40f314b
YaAlex <yaalex.devlab@gmail.com>
2017-04-10 18:29:07 +0200

    fix for more Actionable

--- GameEngine/src/de/uni_kiel/progOOproject17/model/abs/ScreenedBaseModel.java
+++ GameEngine/src/de/uni_kiel/progOOproject17/model/abs/ScreenedBaseModel.java
@@ -3,9 +3,11 @@
  */
 package de.uni_kiel.progOOproject17.model.abs;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.swing.AbstractAction;
 import javax.swing.Action;
 
 import de.uni_kiel.progOOproject17.model.screen.Actionable;
@@ -19,53 +21,54 @@ import de.uni_kiel.progOOproject17.view.abs.Viewable;
  * @since 28.03.2017
  *
  */
-public abstract class ScreenedBaseModel extends TickedBaseModel implements Actionable{
+public abstract class ScreenedBaseModel extends TickedBaseModel implements Actionable {
 
-	
 	private Screen pausedScreen;
 	private Screen activeScreeen;
 	private boolean showPausedScreen;
-	
+
 	private HashMapActionable actions;
-	
 
 	/**
 	 * 
 	 */
 	public ScreenedBaseModel() {
 		actions = new HashMapActionable();
-		
+
 	}
 
-	
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.uni_kiel.progOOproject17.model.abs.TickedBaseModel#tick(long)
 	 */
 	public void tick(long timestamp) {
 		activeScreeen.tick(timestamp);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.uni_kiel.progOOproject17.model.abs.TickedBaseModel#getViewables()
 	 */
 	@Override
 	public Viewable[] getViewables() {
-		
+
 		if (showPausedScreen) {
 			ArrayList<Viewable> views = new ArrayList<>();
-			
+
 			if (pausedScreen != null)
 				views.addAll(Arrays.asList(pausedScreen.getViewables()));
-			views.addAll(Arrays.asList(activeScreeen.getViewables()));
-			
+			views.addAll(Arrays.asList(getActiveScreeen().getViewables()));
+
 			return views.toArray(new Viewable[views.size()]);
 		}
-		return activeScreeen.getViewables();		
+		return getActiveScreeen().getViewables();
 	}
-	
+
 	/**
-	 * Sets the currently active {@link Screen} to s.
-	 * And pauses the last {@link Screen}.
+	 * Sets the currently active {@link Screen} to s. And pauses the last
+	 * {@link Screen}.
 	 * 
 	 * @see ScreenedBaseModel#activeScreeen
 	 * 
@@ -74,17 +77,17 @@ public abstract class ScreenedBaseModel extends TickedBaseModel implements Actio
 	 */
 	public void showScreen(Screen s) {
 		pausedScreen = activeScreeen;
-		activeScreeen = s;	
-		forwardAllActionsToThis(getActiveScreeen());	
+		activeScreeen = s;
+		forwardAllActionsToThis(getActiveScreeen());
 	}
-	
+
 	/**
 	 * @return the activeScreeen
 	 */
 	public Screen getActiveScreeen() {
 		return activeScreeen;
 	}
-	
+
 	/**
 	 * Resumes the paused Screen and pauses the active {@link Screen}.
 	 * 
@@ -93,35 +96,34 @@ public abstract class ScreenedBaseModel extends TickedBaseModel implements Actio
 		showScreen(pausedScreen);
 	}
 
-//	/**
-//	 * Returns a allways up-to-date {@link Action} that performes the
-//	 * {@link Action} corresponding to the key.
-//	 * 
-//	 * @param key
-//	 *            the {@link InputActionKey} which action will be returned
-//	 * @return the action for the key
-//	 */
-//	public Action getAction(InputActionKey key) {
-//
-//		return getActiveScreeen().getAction(key);
-//		
-////		/*
-////		 * Returns only a wrapper action which on being called then will get the
-////		 * corresponding action and call it. This is done so that the returned
-////		 * action will allways be up to date and never as to be refreshed.
-////		 */
-////		return new AbstractAction() {
-////
-////
-////			@Override
-////			public void actionPerformed(ActionEvent e) {
-////				Action a = activeScreeen.getAction(key);
-////				if (a != null)
-////					a.actionPerformed(e);
-////
-////			}
-////		};
-//	}
+	// /**
+	// * Returns a allways up-to-date {@link Action} that performes the
+	// * {@link Action} corresponding to the key.
+	// *
+	// * @param key
+	// * the {@link InputActionKey} which action will be returned
+	// * @return the action for the key
+	// */
+	// public Action getAction(InputActionKey key) {
+	//
+	//
+	//// /*
+	//// * Returns only a wrapper action which on being called then will get the
+	//// * corresponding action and call it. This is done so that the returned
+	//// * action will allways be up to date and never as to be refreshed.
+	//// */
+	//// return new AbstractAction() {
+	////
+	////
+	//// @Override
+	//// public void actionPerformed(ActionEvent e) {
+	//// Action a = activeScreeen.getAction(key);
+	//// if (a != null)
+	//// a.actionPerformed(e);
+	////
+	//// }
+	//// };
+	// }
 
 	/**
 	 * @return
@@ -129,14 +131,14 @@ public abstract class ScreenedBaseModel extends TickedBaseModel implements Actio
 	public boolean isShowPausedScreen() {
 		return showPausedScreen;
 	}
-	
+
 	/**
 	 * @param showPausedScreen
 	 */
 	public void setShowPausedScreen(boolean showPausedScreen) {
 		this.showPausedScreen = showPausedScreen;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -156,7 +158,20 @@ public abstract class ScreenedBaseModel extends TickedBaseModel implements Actio
 	 */
 	@Override
 	public Action getAction(InputActionKey iA) {
-		return actions.getAction(iA);
+		/*
+		 * Returns only a wrapper action which on being called then will get the
+		 * corresponding action and call it. This is done so that the returned
+		 * action will allways be up to date and never as to be refreshed.
+		 */
+		return new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Action a = actions.getAction(iA);
+				if (a != null)
+					a.actionPerformed(e);
+			}
+		};
 	}
 
 	/*
@@ -169,6 +184,5 @@ public abstract class ScreenedBaseModel extends TickedBaseModel implements Actio
 	public void forwardAllActionsToThis(Actionable a) {
 		actions.forwardAllActionsToThis(a);
 	}
-	
-	
+
 }
